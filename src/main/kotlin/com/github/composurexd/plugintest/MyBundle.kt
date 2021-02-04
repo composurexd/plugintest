9eebc8c8fb060553ee20c15cb04c164be7a1e68b
GitHub Action <action@github.com>
2021-02-04 09:00:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/composurexd/plugintest/MyBundle.kt
+++ src/main/kotlin/com/github/composurexd/plugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.composurexd.plugintest
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/composurexd/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/composurexd/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.composurexd.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.composurexd.plugintest.MyBundle
 
 class MyApplicationService {
 
